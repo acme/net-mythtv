ebbab40d14b746180982d83232d92ad2159c57cf
Leon Brocard <acme@astray.com>
2009-04-27 21:22:31 +0100

    go back to parsing the date from the page

--- mightyv.pl
+++ mightyv.pl
@@ -1,7 +1,9 @@
 #!/home/acme/perl-5.10.0/bin/perl
 use strict;
 use warnings;
+use Date::Manip;
 use DateTime;
+use DateTime::Format::Strptime;
 use HTML::TreeBuilder;
 use HTML::TreeBuilder::XPath;
 use LWP::UserAgent;
@@ -10,6 +12,12 @@ use URI;
 use URI::QueryParam;
 
 my $host = 'http://owl.local';
+
+# Thu Apr 30, 2009 (09:00 PM)
+my $datetime_parser = DateTime::Format::Strptime->new(
+    pattern  => '%a %b %d, %Y (%I:%M %p)',
+    on_error => 'croak',
+);
 
 my $ua = LWP::UserAgent->new;
 $ua->default_header( 'Accept-Language' => 'en' );
@@ -24,14 +32,18 @@ my ($table) = $ntree->findnodes('id("listings")');
 foreach my $child ( $table->content_list ) {
     my $class = $child->attr('class');
     next unless $class eq 'scheduled';
-    my @tds   = $child->content_list;
-    my $title = $tds[2]->as_text;
-    my ($actions) = $tds[6]->content_list;
-    my $url = URI->new( $host . $actions->attr('href') );
+    my @tds        = $child->content_list;
+    my $title      = $tds[2]->as_text;
+    my $airdate    = $tds[4]->as_text;
+    my $airdate_dt = $datetime_parser->parse_datetime($airdate);
+    my ($actions)  = $tds[6]->content_list;
+    my $url        = URI->new( $host . $actions->attr('href') );
     die "Missing dontrec field" unless $url->query_param('dontrec') eq 'yes';
     my ( undef, undef, undef, undef, $channel_id, $start_time ) = split '/',
         $url->path;
-    my $dt = DateTime->from_epoch( epoch => $start_time)->set_time_zone('UTC')->set_time_zone('Europe/London');
-    say "$title / $channel_id / $start_time / $dt";
+    my $dt
+        = DateTime->from_epoch( epoch => $start_time )->set_time_zone('UTC')
+        ->set_time_zone('Europe/London');
+    say "$title / $channel_id / $airdate_dt";
 }
 
